8e1af1074aea5545621303b549ce7be481c65bd8
LELO <nyaka.lelo@econocom.com>
2020-07-12 04:13:07 +0200

    Ajout formulaire enregistrement

--- src/app/achattermine/achattermine.component.ts
+++ src/app/achattermine/achattermine.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PanierService } from '../services/panier.service';
 
 @Component({
   selector: 'app-achattermine',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AchattermineComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
+  cartItems = [];
+  frais:number = 3.00;
+  soustotal:number = 0.00;
+  total:number = 0.00;
+  constructor(private formBuilder: FormBuilder,private panier: PanierService) { 
+    this.cartItems = this.panier.getItemsFromCart();
+    this.cartItems.forEach(element => console.log('product price : ',element.prix));
+    this.cartItems.forEach(element => this.soustotal += element.prix);
+    this.total = this.soustotal + this.frais;
+    console.log('Sous total : ',this.soustotal);
+    console.log('cartitems : ',this.cartItems);
+  }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      tel: ['', Validators.required],
+      adr: ['', Validators.required],
+      cb: ['', Validators.required]
+  });
+  }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // si formulaire invalide, on s'arrête.
+      if (this.registerForm.invalid) {
+          return;
+      }
+
+      // s'affiche si les valeurs du formulaires sont valides
+      console.log('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+  }
+
+  onReset() {
+      this.submitted = false;
+      this.registerForm.reset();
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { registerLocaleData } from '@angular/common';
@@ -33,7 +34,8 @@ registerLocaleData(localeFr, 'fr-FR');
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'fr-FR'}
